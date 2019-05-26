3c773a46d46575bd30e1870dcffbae38264fd557
IreneLee0905 <f228300k@gmail.com>
2019-05-27 01:16:00 +1000

    add index

--- src/main/java/irene/web/controller.java
+++ src/main/java/irene/web/controller.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,4 +62,6 @@ public class controller {
       return new ResponseBuilder<>().badRequest(e.getMessage());
     }
   }
+
+
 }
